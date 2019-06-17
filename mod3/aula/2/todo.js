34dd619254f54e34a26a0188470c2a1e9bbc7ae4
unknown <gabriela_gamba@saojose.sc.senai.br>
2019-06-17 12:24:53 -0300

    Aula 2 módulo 3

--- mod3/aula/2/todo.js
+++ mod3/aula/2/todo.js
@@ -1,1 +1,14 @@
-alert("Está funcionando...")
+var listElement =
+document.querySelector("#app ul");
+var inputElement = 
+document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button")
+
+console.log(listElement);
+console.log(inputElement);
+
+var todos = [
+    'Fazer café',
+    'Beber gasolina',
+    'Jogar lixo no lixo'
+];
