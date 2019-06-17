948cf136a6933bf77e6434a5b33d5b143af95247
unknown <gabriela_gamba@saojose.sc.senai.br>
2019-06-17 12:27:08 -0300

    Iniciando aula 3

---
+++ mod3/aula/3/todo.js
@@ -0,0 +1,14 @@
+var listElement =
+document.querySelector("#app ul");
+var inputElement = 
+document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button")
+
+console.log(listElement);
+console.log(inputElement);
+
+var todos = [
+    'Fazer café',
+    'Beber gasolina',
+    'Jogar lixo no lixo'
+];
